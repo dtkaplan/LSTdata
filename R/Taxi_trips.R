99268c2db1d1c0c7b109122e00a2c03375f1f8db
Daniel Kaplan <dtkaplan@gmail.com>
2025-04-28 14:11:01 -0500

    Added Taxi_trips

---
+++ R/Taxi_trips.R
@@ -0,0 +1,22 @@
+#' Taxi trips in New York City
+#'
+#' This is a subset of a much larger data set from the
+#' New York City [Taxi and Limosine Commission](http://www.nyc.gov/html/tlc/html/passenger/taxicab_rate.shtml)
+#' about taxicab trips in New York City.
+
+#' @docType data
+#'
+#' @format Data frame with 9501 rows. Each row is one taxi trip in New York City
+#'
+#' - `distance` the trip distance in miles
+#' - `fare` the amount paid for the trip in US dollars. Unclear if this includes any tip.
+#' - `passengers` the number of passengers on the trip.
+#' - `pickup` the data/time of the start of the triop
+#' - `dropoff` the date/time of the end of the trip.-
+#'
+#' @source Downloaded from the link above in 2015
+#'
+#' @keywords datasets
+#'
+#'
+"Taxi_trips"
